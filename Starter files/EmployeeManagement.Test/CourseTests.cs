80ea0872ede9b6ad3f66b05c0710cff9d14420b1
James Takawira Mukurumbira <jamest@fundi.co.za>
2022-07-25 08:20:04 +0200

    create test project

---
+++ Starter files/EmployeeManagement.Test/CourseTests.cs
@@ -0,0 +1,15 @@
+﻿using EmployeeManagement.DataAccess.Entities;
+
+namespace EmployeeManagement.Test
+{
+    public class CourseTests
+    {
+        [Fact]
+        public void CourseConstructor_ConstructCourse_IsNewMustBeTrue() 
+        {
+            var course = new Course("");
+
+            Assert.True(course.IsNew);
+        }
+    }
+}
---
+++ Starter files/EmployeeManagement.Test/EmployeeTests.cs
@@ -0,0 +1,18 @@
+﻿
+
+using EmployeeManagement.DataAccess.Entities;
+
+namespace EmployeeManagement.Test
+{
+    public class EmployeeTests
+    {
+        [Fact]
+        public void EmployeeFullNamePropertyGetter_InputFirstNameAndLastName_FullNameIsConcatenation() 
+        {
+            var employee = new InternalEmployee("James", "Mukurumbira",0,3000,false,1);
+
+            Assert.Equal("James Mukurumbira", employee.FullName);
+
+        }
+    }
+}
